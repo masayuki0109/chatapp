6369d2ec7aa76ca19170898fe551195da0efed99
Masayuki Yamaji <masayuki@masayuki.local>
2019-05-10 14:01:08 +0900

    actionCableを使ってチャット作成。削除処理実装前

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,11 @@ class RoomsController < ApplicationController
   def show
     @messages = Message.all
   end
+
+  def delete
+    @message = Message.find(params[:id])
+    @message.delete
+    @messages = Message.all
+    render 'show'
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+  after_create_commit { MessageBroadcastJob.perform_later self }
+  after_destroy_commit { MessageBroadcastJob.perform_later self }
 end
